06e1821212c327d4e26eba60aec60b72aa5ecbbc
Andres Gonzales <andrisgonzalis@gmail.com>
2026-06-13 17:18:38 -0500

    Add HCISD Dashboard blog post with Google Drive embed component

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,11 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
+import { GoogleDriveEmbed } from "@/components/mdx/google-drive-embed";
 import type { MDXComponents } from "mdx/types";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    GoogleDriveEmbed,
     ...components,
   };
 }
